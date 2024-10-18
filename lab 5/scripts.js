3c3578a941826efcf90e65c01d3411ab41bae70c
atharvadk <atharvadk27@gmail.com>
2024-10-18 07:33:01 +0530

    commints

--- lab 5/scripts.js
+++ lab 5/scripts.js
@@ -1,86 +1,68 @@
-
-
 $(document).ready(function () {
+    let currentColor = '';
 
-    $(document).on('ready', function () {
-        console.log('Document is ready!');
+    // Function to reset background color
+    function resetBackgroundColors() {
+        $('.hero, #about, #projects, #contact').css('background-color', '');
+    }
+
+    // Change background color on click
+    $('.hero, #about, #projects, #contact').on('click', function () {
+        resetBackgroundColors(); // Reset other sections
+        currentColor = getRandomColor(); // Get a random color
+        $(this).css('background-color', currentColor); // Apply color to clicked section
     });
 
-    $(window).on('load', function () {
-        console.log('Window has loaded!');
-    });
+    // Function to generate a random color
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 
-    $(window).on('resize', function () {
-        console.log('Window has been resized!');
-    });
-
-    $(window).on('scroll', function () {
-        console.log('Window is being scrolled!');
-    });
-
-    $(window).on('unload', function () {
-        alert('You are leaving the page!');
-    });
-
-    $("#home").on('mouseenter', function () {
-        $(this).css('background-color', '#0056b3');
-    });
-
-    $("#home").on('mouseleave', function () {
-        $(this).css('background-color', '');
-    });
-
-    $("#fade-welcome").on('mousedown', function () {
-        $(this).css('background-color', 'gray');
-    });
-
-    $("#fade-welcome").on('mouseup', function () {
-        $(this).css('background-color', '');
-    });
-
+    // Toggle welcome message
     $("#fade-welcome").on('click', function () {
         $("#intro").fadeToggle();
     });
 
-    $("#fade-welcome").on('dblclick', function () {
-        alert('Button double-clicked!');
+    // Project modal display
+    $(".project").on('click', function () {
+        const title = $(this).data('title');
+        const description = $(this).data('description');
+        $("#projectModalLabel").text(title);
+        $("#project-description").text(description);
     });
 
+    // Modify about section
+    $("#modify-about").on('click', function () {
+        $("#about-text").text("I've updated my introduction to reflect my latest skills and experiences.");
+    });
+
+    // Add project description
+    $("#add-project").click(function () {
+        $(".project-list").append('<div class="col-md-6 project" data-toggle="modal" data-target="#projectModal" data-title="New Project" data-description="This is a newly added project."><div class="card"><div class="card-body"><h3 class="card-title">New Project</h3><p class="card-text">This is a newly added project.</p></div></div></div>');
+    });
+
+    // Remove last project
+    $("#remove-project").click(function () {
+        $(".project-list .project").last().remove();
+    });
+
+    // Form submission
     $("#contactForm").on('submit', function (event) {
         event.preventDefault();
         alert("Form Submitted with Name: " + $("#name").val() + " and Email: " + $("#email").val());
     });
 
-    $("#name").on('change', function () {
-        console.log('Name field changed to: ' + $(this).val());
+    // Additional jQuery features
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('bg-light').removeClass('bg-dark');
+        } else {
+            $('.navbar').addClass('bg-dark').removeClass('bg-light');
+        }
     });
-
-    $("#email").on('blur', function () {
-        console.log('Email field lost focus.');
-    });
-
-    $("#email").on('focusin', function () {
-        $(this).css('border-color', '#007bff');
-    });
-
-    $("#message").on('select', function () {
-        console.log('Text in the message box was selected.');
-    });
-
-    $(".project").on('mouseover', function () {
-        $(this).addClass('highlighted');
-    });
-
-    $(".project").on('mouseout', function () {
-        $(this).removeClass('highlighted');
-    });
-
-    $("#add-project").click(function () {
-        $(".project-list").append('<div class="col-md-6 project"><div class="card"><div class="card-body"><h3 class="card-title">New Project</h3><p class="card-text">This is a newly added project.</p></div></div></div>');
-    });
-
-    $("#remove-project").click(function () {
-        $(".project-list .project").last().remove();
-    });
-
 });
